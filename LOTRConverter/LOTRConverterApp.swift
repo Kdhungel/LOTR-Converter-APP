d230a6e9a23b5d5c5f9694f3c7ac1ca998c0f676
Kritish <kritishdhungel@gmail.com>
2024-08-24 11:34:51 +0545

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Kritish Dhungel on 24/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
