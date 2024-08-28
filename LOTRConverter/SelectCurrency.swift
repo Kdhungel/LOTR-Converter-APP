1c7a1ba6da9ee86cc37eca6ca77c024c6084c4c0
Kritish <kritishdhungel@gmail.com>
2024-08-28 12:18:06 +0545

    Laying out select currency screen

---
+++ LOTRConverter/SelectCurrency.swift
@@ -0,0 +1,53 @@
+//
+//  SelectCurrency.swift
+//  LOTRConverter
+//
+//  Created by Kritish Dhungel on 26/08/2024.
+//
+
+import SwiftUI
+
+struct SelectCurrency: View {
+    @Environment(\.dismiss) var dismiss
+
+    var body: some View {
+        ZStack{
+            // background
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+            
+            VStack{
+                // converting from text
+                Text("Select currency you are starting with: ")
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                
+                //currency icons
+                
+                //converting to texts
+                Text("Select currency you would like to converting to: ")
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                   
+                
+                //currency icons
+                //done button
+                
+                Button("Done"){
+                    dismiss()
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.brown)
+                .font(.largeTitle)
+                .padding()
+                .foregroundStyle(.white)
+            }
+            .padding()
+            .multilineTextAlignment(.center)
+        }
+    }
+}
+
+#Preview {
+    SelectCurrency()
+}
